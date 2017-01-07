eac057a357446887e91a54220ac19523073be5d1
KXwonderful <1079292257@qq.com>
2017-01-07 13:20:52 +0800

    add webViewUtils

--- app/src/main/java/com/wonderful/myfirstcode/chapter8/MediaActivity.java
+++ app/src/main/java/com/wonderful/myfirstcode/chapter8/MediaActivity.java
@@ -1,7 +1,7 @@
 package com.wonderful.myfirstcode.chapter8;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
 
